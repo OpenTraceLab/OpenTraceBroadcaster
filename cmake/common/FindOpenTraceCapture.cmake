34849b475e2be8669b7b471759657f0d47d383c9
Carl-Fredrik Sundstrom <carl-fredrik.sundstrom@ericsson.com>
2025-10-27 12:44:21 +0100

    Fix Windows PkgConfig dependency and clean up debugging
    - Make PkgConfig optional on Windows (not available)
    - Remove debugging output from CMake finder and macOS script
    - OpenTraceCapture now found successfully on both platforms

--- cmake/common/FindOpenTraceCapture.cmake
+++ cmake/common/FindOpenTraceCapture.cmake
@@ -32,15 +32,6 @@ find_library(
     "/usr/local/lib/x86_64-linux-gnu"
 )
 
-# Debug output
-if(WIN32)
-  message(STATUS "OpenTraceCapture search paths: C:/Program Files/lib;C:/Program Files/bin")
-  file(GLOB LIB_FILES "C:/Program Files/lib/*opentrace*")
-  message(STATUS "Found files in C:/Program Files/lib: ${LIB_FILES}")
-  file(GLOB BIN_FILES "C:/Program Files/bin/*opentrace*")  
-  message(STATUS "Found files in C:/Program Files/bin: ${BIN_FILES}")
-endif()
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   OpenTraceCapture
