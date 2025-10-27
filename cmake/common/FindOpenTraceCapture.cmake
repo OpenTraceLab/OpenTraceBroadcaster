c9278662665b6d25aa6700163cae4003e425853c
Carl-Fredrik Sundstrom <carl-fredrik.sundstrom@ericsson.com>
2025-10-27 10:38:12 +0100

    Fix gersemi CMake formatting issues
    - Apply gersemi formatting to CMakeLists.txt and FindOpenTraceCapture.cmake
    - Consolidate target_sources to single line
    - Fix multiline function call formatting
    - Remove trailing whitespace

--- cmake/common/FindOpenTraceCapture.cmake
+++ cmake/common/FindOpenTraceCapture.cmake
@@ -12,19 +12,18 @@ if(PKG_CONFIG_FOUND)
   pkg_check_modules(PC_OPENTRACECAPTURE QUIET opentracecapture)
 endif()
 
-find_path(OpenTraceCapture_INCLUDE_DIR
+find_path(
+  OpenTraceCapture_INCLUDE_DIR
   NAMES opentracecapture.h
   PATHS ${PC_OPENTRACECAPTURE_INCLUDE_DIRS}
   PATH_SUFFIXES opentracecapture
 )
 
-find_library(OpenTraceCapture_LIBRARY
-  NAMES opentracecapture
-  PATHS ${PC_OPENTRACECAPTURE_LIBRARY_DIRS}
-)
+find_library(OpenTraceCapture_LIBRARY NAMES opentracecapture PATHS ${PC_OPENTRACECAPTURE_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OpenTraceCapture
+find_package_handle_standard_args(
+  OpenTraceCapture
   REQUIRED_VARS OpenTraceCapture_LIBRARY OpenTraceCapture_INCLUDE_DIR
   VERSION_VAR PC_OPENTRACECAPTURE_VERSION
 )
@@ -32,12 +31,14 @@ find_package_handle_standard_args(OpenTraceCapture
 if(OpenTraceCapture_FOUND)
   set(OpenTraceCapture_LIBRARIES ${OpenTraceCapture_LIBRARY})
   set(OpenTraceCapture_INCLUDE_DIRS ${OpenTraceCapture_INCLUDE_DIR})
-  
+
   if(NOT TARGET OpenTraceCapture::OpenTraceCapture)
     add_library(OpenTraceCapture::OpenTraceCapture UNKNOWN IMPORTED)
-    set_target_properties(OpenTraceCapture::OpenTraceCapture PROPERTIES
-      IMPORTED_LOCATION "${OpenTraceCapture_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${OpenTraceCapture_INCLUDE_DIR}"
+    set_target_properties(
+      OpenTraceCapture::OpenTraceCapture
+      PROPERTIES
+        IMPORTED_LOCATION "${OpenTraceCapture_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${OpenTraceCapture_INCLUDE_DIR}"
     )
   endif()
 endif()
